50c6ea817cf1dfc11a39d61cf94cc9fc365181ed
kemtol <mkemalw@gmail.com>
2018-07-05 15:22:37 +0700

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,1 @@
-/*
-	Author: BestPixels
-	Template: Trav | Travel HTML Landing Page
-	Version: 1.0
-*/
-	
-"use strict";
 
-/*global jQuery, $*/
-jQuery(document).ready(function(){
-	
-	// navigation collapse
-	jQuery('.header-section').parallax("50%", 0.1);
-	jQuery('.spe-offer-section').parallax("50%", 0.1);
-	jQuery('.subscribe-section').parallax("50%", 0.1);
-	
-});
