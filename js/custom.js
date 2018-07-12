5688fbe4d6641ac1661f583d8fc44fd2507ea7fa
kemtol <mkemalw@gmail.com>
2018-07-12 11:55:05 +0700

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,12 @@
 jQuery(window).on('load', function () {
 	//Masonry
-    jQuery('.testimonials-list').masonry({
+	jQuery('.testimonials-list').masonry({
 	  // options
 	  itemSelector: '.testimonial-item',
+	});
+
+	jQuery('.faq-list .row').masonry({
+	  // options
+	  itemSelector: '.faq-item',
 	});
 });
