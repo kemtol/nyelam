f4311e720640053d0855befdb9b64e65987a8902
kemtol <mkemalw@gmail.com>
2018-07-13 17:59:46 +0700

    Update ajax-services.js

--- js/ajax-services.js
+++ js/ajax-services.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
   var servicespot = $(location).attr('pathname');
-  console.log(servicespot);
+  var servlist = servicespot.split(/[\s-]+/);
+  console.log("https://kemtol.github.io/nyelam/ajax/services-" + servlist.[servlist.length-1]);
+  
   $.get("https://kemtol.github.io/nyelam/ajax/services-bali.html", function(data){
     $("#service--list").append(data);
   });
