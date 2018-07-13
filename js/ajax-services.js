908d80e55408edfac9ad4515617d4f5dc0c85d04
kemtol <mkemalw@gmail.com>
2018-07-13 16:02:48 +0700

    Update ajax-services.js

--- js/ajax-services.js
+++ js/ajax-services.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
   $.get("https://kemtol.github.io/nyelam/ajax/services-bali.html", function(data){
-    $("#hotdeal").append(data);
+    $("#service--list").append(data);
   });
 });
