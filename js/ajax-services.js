3154da533c013cc0c88a7284ca294d8bdd2bc922
kemtol <mkemalw@gmail.com>
2018-07-18 15:48:30 +0700

    Update ajax-services.js

--- js/ajax-services.js
+++ js/ajax-services.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  var hostname = "https://kemtol.github.io/nyelam";
+  var hostname = "http://www.e-nyelam.com";
   var servlist = $(location).attr('pathname').split(/[\s-]+/);
   var datasource = hostname + "/ajax/services-" + servlist[servlist.length-1];
   
